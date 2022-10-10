f49244da9966216ac126d4ba127411ed95379c2c
artjitt9611 <artjitt2512@gmail.com>
2022-10-10 11:23:04 +0700

    set engines

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 const router = require("./routes");
+const PORT = process.env.PORT || 5000
 
 
 app.use(cors())
@@ -10,6 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-const listener = app.listen(5000, () => {
+const listener = app.listen(PORT, () => {
 	console.log("Server is running on port " + listener.address().port);
 });
