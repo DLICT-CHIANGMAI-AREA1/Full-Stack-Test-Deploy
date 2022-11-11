b4b07dbce6b295f91f7a48e71663d2dd79ac6dcc
artjitt9611 <artjitt2512@gmail.com>
2022-11-11 17:50:54 +0700

    Delete function by each data

--- Backend/controller/DataStudentEachYear.controller.js
+++ Backend/controller/DataStudentEachYear.controller.js
@@ -1,14 +1,52 @@
 const DataStudent = require("../model/DataStudentEachYear");
 const axios = require("axios");
 
-
 module.exports = {
     FindDataStudentEachYear: async (req, res, next) => {
         try {
-            res.status(200).json(await DataStudent.find());
+            return res.status(200).json(await DataStudent.find());
         } catch (error) {
-            res.status(500).json(error);
+            return res.status(500).json(error.message);
         }
     },
- 
+    AddData: async (req, res, next) => {
+        try {
+            return res.status(200).json(req.body);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    },
+    UpdateData: async (req, res, next) => {
+        try {
+            //console.log(req.body)
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    },
+    DeleteData: async (req, res, next) => {
+        try {
+            const { id, ids } = req.params;
+            console.log(id, ids);
+            return res.status(200).json(
+                await DataStudent.updateOne(
+                    { _id: id },
+                    {
+                        $pull: {
+                            data: { _id: ids },
+                        },
+                    },
+                    { safe: true }
+                )
+            );
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    },
+    AddSemester: async (req, res, next) => {
+        try {
+            console.log(req.body);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    },
 };
--- Backend/routes/DataStudent_each_year.js
+++ Backend/routes/DataStudent_each_year.js
@@ -1,8 +1,12 @@
 const router = require("express").Router()
-const {FindDataStudentEachYear} = require("../controller/DataStudentEachYear.controller");
+const {FindDataStudentEachYear,AddData,UpdateData,DeleteData,AddSemester} = require("../controller/DataStudentEachYear.controller");
 
-
+router.post('/api/AddSemester',AddSemester)
 router.get('/api/FindDataStudentYear',FindDataStudentEachYear)
+router.post('/api/AddData',AddData)
+router.put('/api/UpdateData',UpdateData)
+router.delete('/api/DeleteData/:id/:ids',DeleteData)
+
 
 
 
