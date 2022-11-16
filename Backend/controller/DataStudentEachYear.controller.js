8a20ac5a549da5043e1dfcda48f3ee1a146236c7
artjitt9611 <artjitt2512@gmail.com>
2022-11-16 16:28:43 +0700

    Funtion CRUD Complete

--- Backend/controller/DataStudentEachYear.controller.js
+++ Backend/controller/DataStudentEachYear.controller.js
@@ -11,14 +11,34 @@ module.exports = {
     },
     AddData: async (req, res, next) => {
         try {
-            return res.status(200).json(req.body);
+            const { id } = req.params;
+            const data = req.body;
+            return res.status(200).json(
+                await DataStudent.updateOne(
+                    { _id: id },
+                    {
+                        $push: { data },
+                    },
+                    { safe: true }
+                )
+            );
         } catch (error) {
             return res.status(500).json(error.message);
         }
     },
     UpdateData: async (req, res, next) => {
         try {
-            //console.log(req.body)
+            const { id, ids } = req.params;
+            return res.status(200).json(await DataStudent.updateOne(
+                { _id: id ,"data._id":ids},
+                {
+                    $set: {
+                        "data.$.name": req.body.name,
+                        "data.$.url": req.body.url,
+                     }
+                },
+                { safe: true }
+            ));
         } catch (error) {
             return res.status(500).json(error.message);
         }
@@ -26,7 +46,6 @@ module.exports = {
     DeleteData: async (req, res, next) => {
         try {
             const { id, ids } = req.params;
-            console.log(id, ids);
             return res.status(200).json(
                 await DataStudent.updateOne(
                     { _id: id },
@@ -44,7 +63,21 @@ module.exports = {
     },
     AddSemester: async (req, res, next) => {
         try {
-            console.log(req.body);
+            const data = req.body;
+            let SaveData = new DataStudent(data);
+            await SaveData.save(async (err, data) => {
+                if (err) return res.status(400).json("Bad Request");
+                return res.status(200).json(data);
+            });
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    },
+    DeleteDataRecordByYear: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            await DataStudent.findByIdAndDelete(id);
+            return res.status(200).json(await DataStudent.find());
         } catch (error) {
             return res.status(500).json(error.message);
         }
--- Backend/routes/DataStudent_each_year.js
+++ Backend/routes/DataStudent_each_year.js
@@ -1,11 +1,12 @@
 const router = require("express").Router()
-const {FindDataStudentEachYear,AddData,UpdateData,DeleteData,AddSemester} = require("../controller/DataStudentEachYear.controller");
+const {FindDataStudentEachYear,AddData,UpdateData,DeleteData,AddSemester,DeleteDataRecordByYear} = require("../controller/DataStudentEachYear.controller");
 
 router.post('/api/AddSemester',AddSemester)
 router.get('/api/FindDataStudentYear',FindDataStudentEachYear)
-router.post('/api/AddData',AddData)
-router.put('/api/UpdateData',UpdateData)
+router.post('/api/AddData/:id',AddData)
+router.put('/api/UpdateData/:id/:ids',UpdateData)
 router.delete('/api/DeleteData/:id/:ids',DeleteData)
+router.delete('/api/DeleteDataRecordByYear/:id',DeleteDataRecordByYear)
 
 
 
