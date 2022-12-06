35a524961b167612fc623f16606c019898afbcdc
artjitt9611 <artjitt2512@gmail.com>
2022-12-06 11:57:06 +0700

    Change

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,17 @@
 // configs
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require('cors')
 const router = require("./routes");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-const corsOptions = {
-    origin: "http://localhost:3000",
-    credentials: true,
-};
 
-app.use(cors(corsOptions));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads',express.static('uploads'))
 app.use(router);
 
 const listener = app.listen(PORT, () => {
-    console.log("Server is running on port " + listener.address().port);
+	console.log("Server is running on port " + listener.address().port);
 });
